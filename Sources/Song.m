5617c1e6924babf881f879afd6f05916646e876f
jar3n <jamreng@proton.me>
2026-06-22 02:44:29 -0400

    removed caching stuff because it does not work

--- Sources/Song.m
+++ Sources/Song.m
@@ -21,39 +21,6 @@
     NSURL *url = [NSURL fileURLWithPath:self.filePath];
     AVURLAsset *asset = [AVURLAsset URLAssetWithURL:url options:nil];
 
-    NSArray *keys = @[ @"commonMetadata" ];
-
-    NSError *error = nil;
-    [asset loadValuesAsynchronouslyForKeys:keys completionHandler:^{
-        AVKeyValueStatus status = [asset statusOfValueForKey:@"commonMetadata" error:&error];
-        if (status != AVKeyValueStatusLoaded) { return; }
-
-        NSArray *items = asset.commonMetadata;
-
-        NSString *author = nil;
-        NSString *title = nil;
-
-        for (AVMetadataItem *item in items) {
-            if (![item commonKey]) continue;
-
-            NSString *value = nil;
-            // common keys often come with item.value as an object you can stringify
-            if ([item.value isKindOfClass:[NSString class]]) value = (NSString *)item.value;
-            else if ([item.value respondsToSelector:@selector(stringValue)]) value = [item.value stringValue];
-
-            if (!value) continue;
-
-            if ([item.commonKey isEqualToString:@"artist"]) author = value;
-            if ([item.commonKey isEqualToString:@"title"])  title  = value;
-        }
-
-        dispatch_async(dispatch_get_main_queue(), ^{
-            // use author and title here
-            // self.author = author;
-            // self.songTitle = title;
-        });
-    }];
-
     self.duration = CMTimeGetSeconds(asset.duration);
 
     // AVAsset reads embedded ID3/iTunes-style tags directly from the file,
